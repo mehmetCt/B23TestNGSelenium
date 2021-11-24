c63d1cb83dd333408cefbfa670ca69c40fd61933
Mehmet <mhmtsytrk@gmail.com>
2021-11-24 14:46:40 -0500

    Repository is Created

--- src/test/java/com/cydeo/utils/BrowserUtils.java
+++ src/test/java/com/cydeo/utils/BrowserUtils.java
@@ -24,6 +24,7 @@ public class BrowserUtils {
      * take a name of a test and returns a path to screenshot takes
      */
     public static String getScreenshot(String name) throws IOException {
+
         // name the screenshot with the current date time to avoid duplicate name
         String date = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
         // TakesScreenshot ---> interface from selenium which takes screenshots
