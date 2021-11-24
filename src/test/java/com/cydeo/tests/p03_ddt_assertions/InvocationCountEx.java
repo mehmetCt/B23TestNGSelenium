73232027a7302a8929133fd3ec92f6741716d10a
Mehmet <mhmtsytrk@gmail.com>
2021-11-24 17:09:19 -0500

    p03 is done

---
+++ src/test/java/com/cydeo/tests/p03_ddt_assertions/InvocationCountEx.java
@@ -0,0 +1,12 @@
+package com.cydeo.tests.p03_ddt_assertions;
+
+import org.testng.annotations.Test;
+
+public class InvocationCountEx {
+
+
+    @Test(invocationCount = 10,timeOut = 16000)
+    public void repeatedTest() {
+        System.out.println("Test");
+    }
+}
